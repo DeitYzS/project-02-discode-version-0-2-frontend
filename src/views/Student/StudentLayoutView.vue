10f3f7172ace4a73169bd90f2a95eba70bb0f3c2
Hakushin <kpx1164@gmail.com>
2023-10-23 15:52:16 +0700

    edit picture

--- src/views/Student/StudentLayoutView.vue
+++ src/views/Student/StudentLayoutView.vue
@@ -8,6 +8,8 @@ import type { PropType } from 'vue'
 import TextField from '@/components/TextField.vue'
 import StudentService from '@/services/StudentService'
 import Picker from '@/components/PickerField.vue'
+import ImageUpload from '@/components/ImageUpload.vue'
+
 import type { AdvisorItem } from '@/type'
 const advisors = ref<AdvisorItem[]>([])
 import AdvisorService from '@/services/AdvisorService'
@@ -18,6 +20,7 @@ const editingAdvisor = ref(false)
 const editMode = ref(false)
 const editingName = ref(false)
 const editingLastname = ref(false)
+const editingPicture = ref(false)
 
 const toggleEditMode = () => {
   editMode.value = !editMode.value
@@ -25,6 +28,8 @@ const toggleEditMode = () => {
     editingName.value = false
     editingLastname.value = false
     editingAdvisor.value = false
+    editingPicture.value = false
+
     updateStudent()
   }
 }
@@ -56,6 +61,11 @@ AdvisorService.getAdvisorBy()
   })
 const editingStudentName = () => {
   editingName.value = true
+  editMode.value = true
+}
+
+const editingStudentpicture = () => {
+  editingPicture.value = true
   editMode.value = true
 }
 
@@ -224,13 +234,40 @@ defineProps({
         </div>
         <div class="HStack md:w-1/2 md:justify-end">
           <div class="VStack md:justify-end">
-            <img
-              :key="student?.images[0]"
-              :src="student?.images[0]"
-              alt="student image"
-              class="border-solid border-gray-200 border-2 rounded p-1 m-1 w-40 hover:shadow-lg editable"
-              :class="{ shaking: editMode }"
-            />
+            <div class="VStack">
+              <div v-if="editMode && !editingPicture" class="HStack justify-end">
+                <button class="hover:text-green-500 button-circle" @click="editingStudentpicture">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div v-if="editMode && editingPicture">
+                <!-- <ImageUpload v-model="student.images" /> -->
+                Not available in you region
+              </div>
+
+              <div v-if="!editingPicture">
+                <img
+                  :key="student?.images[0]"
+                  :src="student?.images[0]"
+                  alt="student image"
+                  class="border-solid border-gray-200 border-2 rounded p-1 m-1 w-40 hover:shadow-lg editable"
+                  :class="{ shaking: editMode }"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
