17007a417a444bb2a44b0a468b15a4adf76f2e67
Hakushin <kpx1164@gmail.com>
2023-11-01 05:05:45 +0700

    complete

--- src/components/AnnouncementList.vue
+++ src/components/AnnouncementList.vue
@@ -25,6 +25,17 @@ AnnouncementService.getAnnouncementBy()
   .catch(() => {
     router.push({ name: 'network-error' })
   })
+
+function getFileName(url) {
+  var parts = url.split('/')
+  var lastPart = parts[parts.length - 1]
+  var part2 = decodeURIComponent(lastPart)
+  var part3 = part2.split('=')
+  var part4 = part3[1]
+  var part5 = part4.split('?')
+  var fileName = part5[0]
+  return fileName
+}
 </script>
 
 <template>
@@ -33,14 +44,32 @@ AnnouncementService.getAnnouncementBy()
       <div class="safe-area bubble text-black w-full">
         <p class="text-primary">{{ announcement.title }}</p>
         <p class="text-secondary">{{ announcement.description }}</p>
-        <!-- <div class="" v-if="announcement.files"> -->
         <div v-if="announcement.files">
-          <a href="{{ announcement.files }}"></a>
+          <div class="HStack gap-4 pr-4 m-2">
+            <div v-for="(file, index) in announcement.files" :key="index">
+              <a :href="file" target="_blank" class="secondary-button gap-2">
+                <!-- Display file icon and file name -->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+
+                {{ getFileName(file) }}
+              </a>
+            </div>
+          </div>
         </div>
-        <div v-else>
-          No file
-        </div>
-        <!-- </div> -->
+        <div v-else>No file</div>
       </div>
     </div>
   </div>
@@ -54,6 +83,9 @@ AnnouncementService.getAnnouncementBy()
 }
 .safe-area {
   margin-top: 10px;
+}
+.secondary-button {
+  border-radius: 5px;
 }
 .bubble {
   background-color: white;
